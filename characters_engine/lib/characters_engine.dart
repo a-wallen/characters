ae004fa16cdddbeffb70602f84979d515587b24b
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-08-04 18:44:15 -1000

    chore: remove engine, not necessary

--- characters_engine/lib/characters_engine.dart
+++
@@ -1,4 +0,0 @@
-/// A Very Good Project created by Very Good CLI.
-library characters_engine;
-
-export 'src/characters_engine.dart';
--- characters_engine/lib/src/characters_engine.dart
+++
@@ -1,7 +0,0 @@
-/// {@template characters_engine}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class CharactersEngine {
-  /// {@macro characters_engine}
-  const CharactersEngine();
-}
