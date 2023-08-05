34e1f416f7fdf50eadbb08b35ba63166b62e7c18
Stephen (Alex) Wallen <wallenstephen@outlook.com>
2023-08-04 18:20:15 -1000

    feat: create engine

---
+++ characters_engine/lib/src/characters_engine.dart
@@ -0,0 +1,7 @@
+/// {@template characters_engine}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class CharactersEngine {
+  /// {@macro characters_engine}
+  const CharactersEngine();
+}
---
+++ characters_engine/test/src/characters_engine_test.dart
@@ -0,0 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:characters_engine/characters_engine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('CharactersEngine', () {
+    test('can be instantiated', () {
+      expect(CharactersEngine(), isNotNull);
+    });
+  });
+}
